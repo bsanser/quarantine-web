7b04deafb3ad6a79dfe622079e8eebbe81c2e8d5
Barbara Sanser <bsanser@gmail.com>
2020-03-25 14:18:00 +0000

    feat: created filters list

---
+++ src/components/FiltersList.js
@@ -0,0 +1,100 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import styled from "styled-components";
+import Button from "@material-ui/core/Button";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import InputLabel from "@material-ui/core/InputLabel";
+import { device } from "./../styles/breakpoints";
+
+import CATEGORIES from "./../constants/categories";
+import LANGUAGES from "./../constants/languages";
+import { capitalizeString } from "./../utils/string-utils";
+
+const FiltersContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 16px;
+`;
+
+const StyledFormControl = styled(FormControl)`
+  @media ${device.mobileM} {
+    min-width: 120px;
+  }
+`;
+
+const useStyles = makeStyles(theme => ({
+  select: {
+    background: "transparent"
+  }
+}));
+
+const FiltersList = ({
+  handleApplyFilter,
+  handleFilterByToday,
+  today,
+  category,
+  language
+}) => {
+  const classes = useStyles();
+  return (
+    <FiltersContainer>
+      <Button
+        variant={today ? "contained" : "outlined"}
+        color="primary"
+        name="today"
+        onClick={handleFilterByToday}
+      >
+        Today
+      </Button>
+      <StyledFormControl variant="outlined">
+        <InputLabel htmlFor="category-select">Category</InputLabel>
+        <Select
+          native
+          value={category}
+          onChange={handleApplyFilter}
+          label="Category"
+          inputProps={{
+            name: "category",
+            id: "outlined-category-native-simple"
+          }}
+          className={classes.select}
+        >
+          <option aria-label="None" value="all">
+            All
+          </option>
+          {Object.keys(CATEGORIES).map(cat => (
+            <option key={cat} value={cat}>
+              {cat}
+            </option>
+          ))}
+        </Select>
+      </StyledFormControl>
+      <StyledFormControl variant="outlined">
+        <InputLabel htmlFor="language-select">Language</InputLabel>
+        <Select
+          native
+          value={language}
+          onChange={handleApplyFilter}
+          label="Language"
+          inputProps={{
+            name: "language",
+            id: "outlined-language-native-simple"
+          }}
+          className={classes.select}
+        >
+          <option aria-label="None" value="all">
+            All
+          </option>
+          {Object.keys(LANGUAGES).map(lang => (
+            <option key={lang} value={lang}>
+              {capitalizeString(lang)}
+            </option>
+          ))}
+        </Select>
+      </StyledFormControl>
+    </FiltersContainer>
+  );
+};
+
+export default FiltersList;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { withRouter } from "react-router-dom";
 
 import PlansList from "./PlansList";
@@ -7,6 +7,7 @@ import PlansService from "./../services/PlansService";
 import Fab from "@material-ui/core/Fab";
 import AddIcon from "@material-ui/icons/Add";
 import FormDialog from "./Dialog";
+import FiltersList from "./FiltersList";
 
 const fabStyle = {
   right: 20,
@@ -14,43 +15,77 @@ const fabStyle = {
   position: "fixed"
 };
 
-class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      plans: [],
-      isModalOpen: false
+const Home = () => {
+  const [plans, setPlans] = useState([]);
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [todayFilter, setTodayFilter] = useState(false);
+  const [categoryFilter, setCategoryFilter] = useState("all");
+  const [languageFilter, setLanguageFilter] = useState("all");
+
+  const handleFilterByToday = () => {
+    setTodayFilter(!todayFilter);
+  };
+  const handleApplyFilter = event => {
+    const name = event.target.name;
+    const value = event.target.value;
+    // eslint-disable-next-line default-case
+    switch (name) {
+      case "category":
+        setCategoryFilter(value);
+        break;
+      case "language":
+        setLanguageFilter(value);
+        break;
+    }
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
+
+  useEffect(() => {
+    const fetchPlans = () => {
+      if (
+        !todayFilter &&
+        categoryFilter === "all" &&
+        languageFilter === "all"
+      ) {
+        PlansService.getPlans().then(response => setPlans(response.data));
+        return;
+      }
+      if (categoryFilter !== "all") {
+        PlansService.getPlansByCategory(categoryFilter.toLowerCase()).then(response => {
+          console.log(response.data)
+          setPlans(response.data)
+        }
+         
+        );
+      
+        return;
+      }
     };
-  }
+    fetchPlans();
+  }, [categoryFilter, todayFilter, languageFilter]);
 
-  componentDidMount() {
-    this.fetchPlans();
-  }
-  fetchPlans = () => {
-    PlansService.getPlans().then(response =>
-      this.setState({ plans: response.data })
-    );
-  };
-
-  handleCloseModal = () => {
-    this.setState({ isModalOpen: false });
-  };
-
-  render() {
-    const { plans, isModalOpen } = this.state;
-    return (
-      <div className="Home">
-        <PlansList plans={plans} />
-        <Fab color="primary" aria-label="add" style={fabStyle}>
-          <AddIcon onClick={() => this.setState({ isModalOpen: true })} />
-        </Fab>
-        <FormDialog
-          handleClose={this.handleCloseModal}
-          open={isModalOpen}
-        ></FormDialog>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="Home">
+      <FiltersList
+        handleApplyFilter={handleApplyFilter}
+        handleFilterByToday={handleFilterByToday}
+        category={categoryFilter}
+        language={languageFilter}
+        today={todayFilter}
+      ></FiltersList>
+      <PlansList plans={plans} />
+      <Fab color="primary" aria-label="add" style={fabStyle}>
+        <AddIcon onClick={() => setModalOpen(true)} />
+      </Fab>
+      <FormDialog
+        handleClose={handleCloseModal}
+        open={isModalOpen}
+      ></FormDialog>
+    </div>
+  );
+};
 
 export default withRouter(Home);
