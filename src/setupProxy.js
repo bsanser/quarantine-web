c5f2f026a0dd52df8c8ad6ef941f54e281fb2a34
Barbara Sanser <bsanser@gmail.com>
2020-03-29 16:30:24 +0100

    added config proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,13 @@
 const proxy = require("http-proxy-middleware");
 
+let url;
+
+if (process.env.NODE_ENV === "production") {
+  url = "https://pacific-island-46723.herokuapp.com";
+} else {
+  url = "http://localhost:3001";
+}
+
 module.exports = function(app) {
-  app.use(proxy(["/api", "/auth/google"], { target: "http://localhost:3001" }));
+  app.use(proxy(["/api", "/auth/google"], { target: url }));
 };
